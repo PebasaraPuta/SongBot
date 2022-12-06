cc220948e8f57475ab12803c512f2fd4196e6674
Prabhasha :) <117509567+PebasaraPuta@users.noreply.github.com>
2022-12-06 04:41:27 -0800

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import re
 from youtube_dl import YoutubeDL
 
 class Config:
-    APP_ID = "19225868"
-    API_HASH = "60c8b4510a12fb593dd775409b9f43d2"
-    BOT_TOKEN = "5734386228:AAHw0AUoUQlzrfliSTI1Es87O_F0ngrNO9k"
+    APP_ID = "2192067"
+    API_HASH = "d2e0ba99f1b9cdb632b43633edb76f11"
+    BOT_TOKEN = "5851948681:AAGCEfMnl5LTxI4dctfsOZOJdzJsg2PZzw0"
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nI'm A Simple Music Downloader Bot,</b>\n\nJust send me Any Songs name to download it ")
     START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/5f8dce1f31eab05963186.jpg")
     OWNER = os.environ.get("OWNER", "Prabha_sha") 
